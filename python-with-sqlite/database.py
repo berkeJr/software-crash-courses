a60de65c727e3730fa33e8fd48e6e80a2627794f
Berke Sayın <sayinberkesayin@gmail.com>
2022-01-13 18:08:49 +0300

    Add multile records.

--- python-with-sqlite/database.py
+++ python-with-sqlite/database.py
@@ -28,11 +28,23 @@ c = conn.cursor()
 
 
 # Create another cursor command to insert a record inside the table
-c.execute("INSERT INTO customers VALUES ('John', 'Elder', 'john@test.com')")
+# c.execute("INSERT INTO customers VALUES ('John', 'Elder', 'john@test.com')")
+# c.execute("INSERT INTO customers VALUES ('Berke', 'Sayin', 'berke@test.com')")
+# c.execute("INSERT INTO customers VALUES ('Mehdi', 'Ay', 'mehdi@test.com')")
+# c.execute("INSERT INTO customers VALUES ('Hamza', 'Gunduz', 'hamza@test.com')")
+# c.execute("INSERT INTO customers VALUES ('Selcuk', 'Yilmaz', 'selcuk@test.com')")
+# c.execute("INSERT INTO customers VALUES ('Samet', 'Can', 'samet@test.com')")
+# c.execute("INSERT INTO customers VALUES ('Sami', 'Ince', 'sami@test.com')")
+# c.execute("INSERT INTO customers VALUES ('Hasan', 'Ozdogan', 'john@test.com')")
 
 
+# If we want to add multiple records: 
+many_customers = [('Wes', 'Brown', 'wes@test.com'), 
+                  ('Steph', 'Kuewa', 'steph@test.com'), 
+                  ('Ahmet', 'Sayin', 'ahmet@test.com'),
+                 ]
 
-
+c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
 
 # we need to commit our command
 conn.commit()
